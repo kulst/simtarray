f9ca2ea37df59a9648b0fa914d6f7074a2aad830
kulst <60887784+kulst@users.noreply.github.com>
2025-07-15 18:54:11 +0200

    Simplified return types

--- simtarray/src/lib.rs
+++ simtarray/src/lib.rs
@@ -1,8 +1,6 @@
 #![no_std]
 #![cfg_attr(target_arch = "nvptx64", feature(stdarch_nvptx))]
 
-use crate::size_type::*;
-
 pub struct SimtArray<T, Sc, I, L: Layout, Sh: Shape> {
     ptr: *mut T,
     layout: PhantomData<L>,
@@ -11,44 +9,24 @@ pub struct SimtArray<T, Sc, I, L: Layout, Sh: Shape> {
     state: PhantomData<I>,
 }
 
-pub struct SimtArrayViewMut<'a, T, Sc: SyncableScope, Sh: Shape, L: Layout> {
-    inner: ViewMut<'a, T, Sh, L>,
+pub struct SimtArrayMutRef<'a, T, Sc: SyncableScope, L: Layout, Sh: Shape> {
+    view: Option<(*mut T, L::Mapping<Sh>)>,
+    layout: PhantomData<&'a mut L>,
     scope: PhantomData<Sc>,
 }
 
-impl<'a, T, Sc: SyncableScope, L: Layout, Sh: Shape> Deref for SimtArrayViewMut<'a, T, Sc, Sh, L> {
-    type Target = ViewMut<'a, T, Sh, L>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.inner
+impl<'a, T, Sc: SyncableScope, L: Layout, Sh: Shape> SimtArrayMutRef<'a, T, Sc, L, Sh> {
+    pub fn expr_mut<'b>(&'b mut self) -> Option<ViewMut<'b, T, Sh, L>>
+    where
+        'a: 'b,
+    {
+        self.view
+            .as_ref()
+            .map(|view| unsafe { ViewMut::new_unchecked(view.0, view.1.clone()) })
     }
 }
 
-impl<'a, T, Sc: SyncableScope, L: Layout, Sh: Shape> DerefMut
-    for SimtArrayViewMut<'a, T, Sc, Sh, L>
-{
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.inner
-    }
-}
-
-impl<'a, T, Sc: SyncableScope, L: Layout, Sh: Shape> AsRef<ViewMut<'a, T, Sh, L>>
-    for SimtArrayViewMut<'a, T, Sc, Sh, L>
-{
-    fn as_ref(&self) -> &ViewMut<'a, T, Sh, L> {
-        &self.inner
-    }
-}
-
-impl<'a, T, Sc: SyncableScope, L: Layout, Sh: Shape> AsMut<ViewMut<'a, T, Sh, L>>
-    for SimtArrayViewMut<'a, T, Sc, Sh, L>
-{
-    fn as_mut(&mut self) -> &mut ViewMut<'a, T, Sh, L> {
-        &mut self.inner
-    }
-}
-
-impl<'a, T, Sc: SyncableScope, L: Layout, Sh: Shape> Drop for SimtArrayViewMut<'a, T, Sc, Sh, L> {
+impl<'a, T, Sc: SyncableScope, L: Layout, Sh: Shape> Drop for SimtArrayMutRef<'a, T, Sc, L, Sh> {
     fn drop(&mut self) {
         unsafe { Sc::sync() };
     }
@@ -79,9 +57,9 @@ where
     pub fn expr<'a>(&'a self) -> View<'a, T, Sh, L> {
         unsafe { View::new_unchecked(self.ptr as *const T, self.mapping.clone()) }
     }
-    pub fn expr_mut<'a>(&'a mut self) -> ViewMut<'a, T, Sh, L> {
-        unsafe { ViewMut::new_unchecked(self.ptr, self.mapping.clone()) }
-    }
+    // pub fn expr_mut<'a>(&'a mut self) -> ViewMut<'a, T, Sh, L> {
+    //     unsafe { ViewMut::new_unchecked(self.ptr, self.mapping.clone()) }
+    // }
 }
 type Unsigned<Sc> = <<<Sc as Scope>::Arch as Arch>::IndexSize as SizeType>::Unsigned;
 
@@ -89,17 +67,10 @@ impl<T, Sc, I: Splitable + Viewable, L: Layout, D0: Dim> SimtArray<T, Sc, I, L, 
 where
     Sc: Scope,
 {
-    pub fn view_with_limited_quantity<'a, R: RangeBounds<usize>, E, Ps>(
+    pub fn view_with_limited_quantity<'a, E, Ps>(
         &'a self,
         quantity: Unsigned<Sc>,
-    ) -> Option<
-        View<
-            'a,
-            T,
-            <(StepRange<R, isize>,) as ViewIndex>::Shape<(D0,)>,
-            <(StepRange<R, isize>,) as ViewIndex>::Layout<L>,
-        >,
-    >
+    ) -> Option<View<'a, T, (usize,), Strided>>
     where
         E: Scope<Arch = Sc::Arch>,
         Ps: ProjectionSetDim0<(D0,), E, Sc, Arch = Sc::Arch>,
@@ -125,18 +96,10 @@ where
             )
         }
     }
-    pub fn view_mut_with_limited_quantity<'a, R: RangeBounds<usize>, E, Ps>(
+    pub fn view_mut_with_limited_quantity<'a, E, Ps>(
         &'a mut self,
         quantity: Unsigned<Sc>,
-    ) -> Option<
-        SimtArrayViewMut<
-            'a,
-            T,
-            Sc,
-            <(StepRange<R, isize>,) as ViewIndex>::Shape<(D0,)>,
-            <(StepRange<R, isize>,) as ViewIndex>::Layout<L>,
-        >,
-    >
+    ) -> SimtArrayMutRef<'a, T, Sc, Strided, (usize,)>
     where
         Sc: SyncableScope,
         E: UnitScope<Arch = Sc::Arch>,
@@ -144,34 +107,29 @@ where
     {
         let dim0 = Ps::dim0();
         let idx0 = Ps::idx0();
-        if idx0.as_() > self.mapping.dim(0) {
-            return None;
-        }
-        if idx0 >= quantity {
-            return None;
+        if idx0.as_() > self.mapping.dim(0) || idx0 >= quantity {
+            return SimtArrayMutRef {
+                view: None,
+                scope: PhantomData,
+                layout: PhantomData,
+            };
         }
         let quantity = dim0.min(quantity);
-        unsafe {
-            Some(SimtArrayViewMut {
-                inner: ViewMut::<_, (D0,), L>::new_unchecked(self.ptr, self.mapping.clone())
-                    .into_view(StepRange {
-                        range: idx0.as_()..,
-                        step: quantity.as_() as isize,
-                    }),
-                scope: PhantomData,
-            })
+        let view_mut = unsafe {
+            ViewMut::<_, (D0,), L>::new_unchecked(self.ptr, self.mapping.clone()).into_view(
+                StepRange {
+                    range: idx0.as_()..,
+                    step: quantity.as_() as isize,
+                },
+            )
+        };
+        SimtArrayMutRef {
+            view: Some(view_mut.into_raw_parts()),
+            layout: PhantomData,
+            scope: PhantomData,
         }
     }
-    pub fn view<'a, R: RangeBounds<usize>, E, Ps>(
-        &'a self,
-    ) -> Option<
-        View<
-            'a,
-            T,
-            <(StepRange<R, isize>,) as ViewIndex>::Shape<(D0,)>,
-            <(StepRange<R, isize>,) as ViewIndex>::Layout<L>,
-        >,
-    >
+    pub fn view<'a, E, Ps>(&'a self) -> Option<View<'a, T, (usize,), Strided>>
     where
         E: Scope<Arch = Sc::Arch>,
         Ps: ProjectionSetDim0<(D0,), E, Sc, Arch = Sc::Arch>,
@@ -193,18 +151,12 @@ where
             )
         }
     }
-    pub fn view_mut<'a, R: RangeBounds<usize>, E, Ps>(
+    pub fn view_mut<'a, 'b, 'c, E, Ps>(
         &'a mut self,
-    ) -> Option<
-        SimtArrayViewMut<
-            'a,
-            T,
-            Sc,
-            <(StepRange<R, isize>,) as ViewIndex>::Shape<(D0,)>,
-            <(StepRange<R, isize>,) as ViewIndex>::Layout<L>,
-        >,
-    >
+    ) -> SimtArrayMutRef<'a, T, Sc, Strided, (usize,)>
     where
+        'a: 'b,
+        'b: 'c,
         Sc: SyncableScope,
         E: UnitScope<Arch = Sc::Arch>,
         Ps: ProjectionSetDim0<(D0,), E, Sc, Arch = Sc::Arch>,
@@ -212,25 +164,32 @@ where
         let dim0 = Ps::dim0();
         let idx0 = Ps::idx0();
         if idx0.as_() > self.mapping.dim(0) {
-            return None;
-        }
-        unsafe {
-            Some(SimtArrayViewMut {
-                inner: ViewMut::<_, (D0,), L>::new_unchecked(self.ptr, self.mapping.clone())
-                    .into_view(StepRange {
-                        range: idx0.as_()..,
-                        step: dim0.as_() as isize,
-                    }),
+            return SimtArrayMutRef {
+                view: None,
                 scope: PhantomData,
-            })
+                layout: PhantomData,
+            };
+        }
+        let view_mut = unsafe {
+            ViewMut::<_, (D0,), L>::new_unchecked(self.ptr, self.mapping.clone()).into_view(
+                StepRange {
+                    range: idx0.as_()..,
+                    step: dim0.as_() as isize,
+                },
+            )
+        };
+        SimtArrayMutRef {
+            view: Some(view_mut.into_raw_parts()),
+            layout: PhantomData,
+            scope: PhantomData,
         }
     }
 }
 
 impl<T, Sc, I, L: Layout, D0: Dim> SimtArray<T, Sc, I, L, (D0,)>
 where
-    Sc: Scope + Clone,
-    I: Viewable,
+    Sc: Scope,
+    I: Splitable,
 {
     /// # Safety
     /// Must be called in a kernel uniform control flow state
@@ -319,19 +278,14 @@ mod init_state;
 
 mod size_type;
 
-use core::{
-    cell::UnsafeCell,
-    marker::PhantomData,
-    mem::MaybeUninit,
-    ops::{Deref, DerefMut, RangeBounds},
-};
+use core::{cell::UnsafeCell, marker::PhantomData, mem::MaybeUninit};
 
 pub use archs::*;
 pub use init_state::*;
 use mdarray::{
-    Const, Dim, Layout, Mapping, Shape, Slice, StepRange, Strided, View, ViewMut,
-    expr::{Expression, enumerate, for_each},
-    index::ViewIndex,
+    Dim, Layout, Mapping, Shape, StepRange, Strided, View, ViewMut,
+    expr::{enumerate, for_each},
 };
+pub use size_type::*;
 
 pub(crate) mod util;
